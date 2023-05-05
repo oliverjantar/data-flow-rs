8fa291e58548a66a934602ddf2c672c14f5ff308
jiriandras <jiriandras@protonmail.com>
2023-05-05 10:25:20 +0200

    Simple stream of a file

--- src/bin/executor.rs
+++ src/bin/executor.rs
@@ -4,10 +4,25 @@ use async_std::{fs::File, io::BufReader, prelude::*};
 
 fn main() {
     println!("running");
+    run_example_1().unwrap();
 }
 
 trait Outbound {
-    fn send(&self) {}
+    fn send(&self, line: String);
+}
+
+struct PrintOutbound {}
+
+impl Outbound for PrintOutbound {
+    fn send(&self, line: String) {
+        println!("{}", line);
+    }
+}
+
+impl PrintOutbound {
+    fn new() -> Self {
+        Self {}
+    }
 }
 
 async fn run<T>(source: T, outbound: Box<dyn Outbound>) -> Result<(), Box<dyn Error>>
@@ -17,8 +32,21 @@ where
     let mut lines = BufReader::new(source).lines();
 
     while let Some(line) = lines.next().await {
-        outbound.send()
+        // let line = line?;
+        outbound.send(line?)
     }
+
+    Ok(())
+}
+
+fn run_example_1() -> Result<(), Box<dyn Error>> {
+    async_std::task::block_on(run_test())
+}
+
+async fn run_test() -> Result<(), Box<dyn Error>> {
+    let source = File::open("./data/1").await?;
+    let outbound = Box::new(PrintOutbound::new());
+    async_std::task::block_on(run(source, outbound))?;
 
     Ok(())
 }
