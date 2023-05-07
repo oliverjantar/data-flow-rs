5ab2ab617d132ab6da6738e5b935ab0808a5f31a
jiriandras <jiriandras@protonmail.com>
2023-05-07 21:34:13 +0200

    Add generic structs for data module sender, receiver example

--- src/bin/executor.rs
+++ src/bin/executor.rs
@@ -1,20 +1,24 @@
 use std::error::Error;
 
+use async_std::io::prelude::*;
+use async_std::task;
 use async_std::{fs::File, io::BufReader, prelude::*};
+use tokio::sync::broadcast;
 
 fn main() {
-    run_example_1().unwrap();
+    //  run_example_1().unwrap();
+    run_example_2().unwrap();
 }
 
-trait Outbound {
-    fn send(&self, line: String);
+trait Outbound<T> {
+    fn send(&self, packet: T);
 }
 
 struct PrintOutbound {}
 
-impl Outbound for PrintOutbound {
-    fn send(&self, line: String) {
-        println!("{}", line);
+impl Outbound<String> for PrintOutbound {
+    fn send(&self, packet: String) {
+        println!("{}", packet);
     }
 }
 
@@ -24,7 +28,30 @@ impl PrintOutbound {
     }
 }
 
-async fn run<T>(source: T, outbound: Box<dyn Outbound>) -> Result<(), Box<dyn Error>>
+struct DataModuleSender<T> {
+    sender: broadcast::Sender<T>,
+}
+
+impl<T> DataModuleSender<T>
+where
+    T: Clone,
+{
+    fn new() -> Self {
+        let (sender, _recv) = broadcast::channel(100);
+
+        Self { sender }
+    }
+}
+
+impl<T> Outbound<T> for DataModuleSender<T> {
+    fn send(&self, packet: T) {
+        if self.sender.send(packet).is_err() {
+            eprintln!("Couldn't send packet to sender.");
+        }
+    }
+}
+
+async fn run<T>(source: T, outbound: Box<dyn Outbound<String>>) -> Result<(), Box<dyn Error>>
 where
     T: Unpin + Sized + async_std::io::Read,
 {
@@ -39,13 +66,35 @@ where
 }
 
 fn run_example_1() -> Result<(), Box<dyn Error>> {
-    async_std::task::block_on(run_test())
+    async_std::task::block_on(run_test_1())
 }
 
-async fn run_test() -> Result<(), Box<dyn Error>> {
+async fn run_test_1() -> Result<(), Box<dyn Error>> {
     let source = File::open("./data/1").await?;
     let outbound = Box::new(PrintOutbound::new());
     run(source, outbound).await?;
+
+    Ok(())
+}
+
+fn run_example_2() -> Result<(), Box<dyn Error>> {
+    async_std::task::block_on(run_test_2())
+}
+
+async fn run_test_2() -> Result<(), Box<dyn Error>> {
+    let source = File::open("./data/2").await?;
+    let sender = DataModuleSender::<String>::new();
+
+    let mut receiver = sender.sender.subscribe();
+
+    let handle = task::spawn_local(async move {
+        while let Ok(value) = receiver.recv().await {
+            println!("Receiver: {}", value);
+        }
+    });
+
+    run(source, Box::new(sender)).await?;
+    handle.await;
 
     Ok(())
 }
